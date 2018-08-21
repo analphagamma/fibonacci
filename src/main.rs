2def2d0e56475ce90b2f6abd98f32175bee8b4fd
Peter Bocz <bocz.petya@gmail.com>
2018-08-21 22:43:47 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,10 @@ fn main() {
         .parse()
         .expect("Not a number!");
 
-    println!("Fibonacci value: {}", fibo(elem));
-    
+    println!("Fibonacci value: {}", fibo(elem));   
 }
 
 fn fibo(n: u64) -> u64 {
-
     match n {
         0 => 0,
         1 => 1,
